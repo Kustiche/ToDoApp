338975ac5eb60aa15cce593518e56fd9f1475b5b
Kustiche <ChernichnyKustik@mail.ru>
2026-07-09 17:02:56 +0300

    Исправил функцию подготовки модалки перед открытием

--- js/modal/prepareModal.js
+++ js/modal/prepareModal.js
@@ -13,31 +13,33 @@ import {
 
 export function prepareModal(taskText) {
   const editingInput = modalEditingTask.querySelector('.modal__input-text');
+  const innerTask = taskText.closest('.todo__task');
 
   editingInput.value = taskText.textContent;
 
-  tasks.forEach((task) => {
+  tasks.forEach((task, id) => {
     const isTask = task.text === taskText.textContent;
+    const isTaskId = id === Number(innerTask.id);
     const isTaskDone = task.status === STATUSES.DONE;
     const isTaskInProgress = task.status === STATUSES.IN_PROGRESS;
     const isTaskHigh = task.priority === PRIORITIES.HIGH;
     const isTaskLow = task.priority === PRIORITIES.LOW;
 
     modalBtns.forEach((btn) => {
-      if (isTask) {
+      if (isTask && isTaskId) {
         btn.style.backgroundColor = '';
       }
     });
 
-    if (isTask && isTaskDone) {
+    if (isTask && isTaskId && isTaskDone) {
       modalBtnDone.style.backgroundColor = COLORS.SHUTTLE_GRAY;
-    } else if (isTask && isTaskInProgress) {
+    } else if (isTask && isTaskId && isTaskInProgress) {
       modalBtnInProgress.style.backgroundColor = COLORS.SHUTTLE_GRAY;
     }
 
-    if (isTask && isTaskHigh) {
+    if (isTask && isTaskId && isTaskHigh) {
       modalBtnHigh.style.backgroundColor = COLORS.SHUTTLE_GRAY;
-    } else if (isTask && isTaskLow) {
+    } else if (isTask && isTaskId && isTaskLow) {
       modalBtnLow.style.backgroundColor = COLORS.SHUTTLE_GRAY;
     }
   });
