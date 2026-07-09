2cc1c3830a252747f6f1e882c96b017653ddb33b
Kustiche <ChernichnyKustik@mail.ru>
2026-07-09 18:48:07 +0300

    Зарефакторил функцию смены приоритета

--- js/modal/changePriority.js
+++ js/modal/changePriority.js
@@ -7,8 +7,8 @@ export function changePriority(target, task) {
   const isBtnHigh = target.textContent === 'Высокий приоритет';
   const isBtnLow = target.textContent === 'Низкий приоритет';
 
-  tasks.forEach((el, id) => {
-    const isIdTask = id === Number(task.id);
+  tasks.forEach((el) => {
+    const isIdTask = el.id === Number(task.id);
 
     if (isIdTask && isBtnHigh) {
       el.priority = PRIORITIES.HIGH;
